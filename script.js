141a9ff90253d35817b165ab5b07c5f21d3163ac
Scott <156713928+slerbert@users.noreply.github.com>
2024-09-22 02:31:56 +1000

    Add function to generate grid from user input Add event listener to New Grid button with handler that parses and validates input. Input is then fed into createGrid function.

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
+// Get reference to CSS stylesheet to alter square class
 const stylesheet = document.styleSheets[0];
 const squareRulesCSS = [...stylesheet.cssRules].find(
     (rule) => rule.selectorText === '.square'
 );
 
 const squareContainer = document.querySelector('.squares-container');
+const newGridButton = document.querySelector('#new-grid');
 
+newGridButton.addEventListener('click', getNewGridSpec);
+
+function getNewGridSpec() {
+    let keepGoing = true;
+    let newGridSpec = parseInt(prompt('Please enter the number of squares you\'d like per side of new grid.', 16));
+
+    while (keepGoing) {
+        if (
+            newGridSpec < 4 
+            || newGridSpec > 100 
+            || !Number.isInteger(newGridSpec)
+        ) {
+            newGridSpec = parseInt(prompt('Please enter a number between 4 and 100.', 16));
+        } else {
+            createGrid(newGridSpec);
+            keepGoing = false;
+        }
+    }
+}
 
 function createGrid(num) {
     let squareWidth = (1/num) * 100;
     squareRulesCSS.style.setProperty("width", `${squareWidth}%`);
     squareRulesCSS.style.setProperty("height", `${squareWidth}%`);
+
+    // Remove all squares from squareContainer
+    squareContainer.replaceChildren();
 
     for (let i = 0; i < num*num; i++) {
         let square = document.createElement('div');
@@ -18,5 +42,5 @@ function createGrid(num) {
     }
 }
 
-createGrid(10);
-
+// Initialize grid with 16 squares
+createGrid(16);
