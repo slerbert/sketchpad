d1b9b66786596c367f0807a08593ac73a2292a15
Scott <156713928+slerbert@users.noreply.github.com>
2024-09-22 17:13:37 +1000

    Refactor code Cleaned up validation process within getNewGridSpec Added isValidSpec function to encapsulate the validation of user input Changed function name 'randomRange' to 'randomIntBetween' Added functionality to increase opacity of square by 10% at each mouseover event

--- script.js
+++ script.js
@@ -1,40 +1,29 @@
+const squareContainer = document.querySelector('.squares-container');
+const newGridButton = document.querySelector('#new-grid');
+
 // Get reference to CSS stylesheet to alter square class
 const stylesheet = document.styleSheets[0];
 const squareRulesCSS = [...stylesheet.cssRules].find(
     (rule) => rule.selectorText === '.square'
 );
 
-const squareContainer = document.querySelector('.squares-container');
-const newGridButton = document.querySelector('#new-grid');
+function isValidSpec(gridSpec) {
+    let result = 
+        gridSpec >= 2 
+        && gridSpec <= 100 
+        && Number.isInteger(gridSpec);
 
-function randomRange(min, max) {
-    return Math.floor(Math.random() * (max-min)) + min;
-}
-
-function randomColor() {
-    return `rgb(
-        ${randomRange(0, 255)},
-        ${randomRange(0, 255)},
-        ${randomRange(0, 255)}
-    )`;
+    return result;
 }
 
 function getNewGridSpec() {
-    let keepGoing = true;
     let newGridSpec = parseInt(prompt('Please enter the number of squares you\'d like per side of new grid.', 16));
 
-    while (keepGoing) {
-        if (
-            newGridSpec < 4 
-            || newGridSpec > 100 
-            || !Number.isInteger(newGridSpec)
-        ) {
-            newGridSpec = parseInt(prompt('Please enter a number between 4 and 100.', 16));
-        } else {
-            createGrid(newGridSpec);
-            keepGoing = false;
-        }
+    while (!isValidSpec(newGridSpec)) {
+        newGridSpec = parseInt(prompt('Please enter a number between 2 and 100.', 16));
     }
+
+    createGrid(newGridSpec);
 }
 
 function createGrid(num) {
@@ -55,8 +44,26 @@ function createGrid(num) {
 newGridButton.addEventListener('click', getNewGridSpec);
 
 squareContainer.addEventListener('mouseover', (event) => {
+    const squareOpacity = Number(event.target.style.opacity);
+
+    if (squareOpacity < 1) {
+        event.target.style.opacity = squareOpacity + 0.1;
+    }
+
     event.target.style.backgroundColor = randomColor();
 });
+
+function randomIntBetween(min, max) {
+    return Math.floor(Math.random() * (max-min)) + min;
+}
+
+function randomColor() {
+    return `rgb(
+        ${randomIntBetween(0, 255)},
+        ${randomIntBetween(0, 255)},
+        ${randomIntBetween(0, 255)}
+    )`;
+}
 
 // Initialize grid with 16 squares
 createGrid(16);
